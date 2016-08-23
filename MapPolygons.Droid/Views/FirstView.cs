dff2c490c09c4eae465504d207a1237bdc096618
Vasiliy Uchanev <Vasily@sharp-dev.net>
2016-08-23 13:26:24 +0500

    yup. it works

--- MapPolygons.Droid/Views/FirstView.cs
+++ MapPolygons.Droid/Views/FirstView.cs
@@ -1,5 +1,7 @@
 using Android.App;
 using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Graphics;
 using Android.OS;
 using Android.Views;
 using MvvmCross.Droid.Views;
@@ -22,6 +24,18 @@ namespace MapPolygons.Droid.Views
 		public void OnMapReady(GoogleMap googleMap)
 		{
 			_map = googleMap;
+
+			// Instantiates a new Polygon object and adds points to define a rectangle
+			var rectOptions = new PolygonOptions()
+						  .Add(new LatLng(9,10),
+							   new LatLng(10, 11),
+							   new LatLng(11, 10),
+							   new LatLng(10, 9));
+			rectOptions.InvokeFillColor(0x66FF0000);
+			rectOptions.InvokeStrokeColor(0x660000FF);
+
+			// Get back the mutable Polygon
+			var polygon = _map.AddPolygon(rectOptions);
 		}
 	}
 }
